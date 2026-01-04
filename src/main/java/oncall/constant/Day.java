5b88eb5f2d360890081349411099b5be8591bcb5
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-01-04 14:51:56 +0900

    feat: 요일, 평일, 휴일 상수화

--- src/main/java/oncall/constant/Day.java
+++ src/main/java/oncall/constant/Day.java
@@ -1,4 +1,19 @@
 package oncall.constant;
 
 public enum Day {
+    MONDAY("월", Type.WEEKDAY),
+    TUESDAY("화", Type.WEEKDAY),
+    WEDNESDAY("수",Type.WEEKDAY),
+    THURSDAY("목", Type.WEEKDAY),
+    FRIDAY("금", Type.WEEKDAY),
+    SATURDAY("토", Type.WEEKEND),
+    SUNDAY("일",Type.WEEKEND);
+
+    private final String day;
+    private final Type type;
+
+    Day(String day, Type type) {
+        this.day = day;
+        this.type = type;
+    }
 }
